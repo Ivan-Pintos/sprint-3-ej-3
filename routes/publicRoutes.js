211415a6bbb1aece56fd0622d4e4de8ddc7b47a6
RamiroVidal26 <ramirovidalc@gmail.com>
2023-05-05 16:23:25 -0300

    vista admin

--- routes/publicRoutes.js
+++ routes/publicRoutes.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const pagesController = require("../controllers/pagesController");
+const articleController = require("../controllers/articleController");
+
+const { Article } = require("../models");
 // Rutas relacionadas a la parte pública del sitio web:
 // ...
 router.get("/", pagesController.showHome);
+router.get("/admin", articleController.showAdmin);
+
 module.exports = router;
